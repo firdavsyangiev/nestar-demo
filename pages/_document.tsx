66685977cd33953efe9a30a36c83dbcba83b79fd
Ray <firdavsyangiev@gmail.com>
2026-09-15 11:25:42 +0900

    feat: completed homepage publishing and develop apollo client connection

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,20 +6,19 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="title" content="Nestar Demo" />
-        <meta name="robots" content="index, follow" />
-        <link rel="stylesheet" href="/img/logo/favicon.svg" />
-        <link rel="icon" type="image/png" href="/img/logo/favicon.svg " />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
 
         {/* SEO */}
         <meta
-          name="keywords"
+          name="keyword"
           content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
         />
         <meta
           name="description"
           content={
             "Buy and sell properties anywhere anytime in South Korea. | " +
-            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. |" +
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время. | " +
             "대한민국 언제 어디서나 부동산을 사고팔 수 있습니다."
           }
         />
